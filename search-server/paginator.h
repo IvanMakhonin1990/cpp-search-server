da866633709d50dfba66099a2c37ca447392143c
IvanMakhonin <88058673+IvanMakhonin@users.noreply.github.com>
2022-03-18 14:27:58 +0300

    Resolved comments

--- search-server/paginator.h
+++ search-server/paginator.h
@@ -32,8 +32,8 @@ template <typename Iterator> class Paginator {
 public:
   Paginator(Iterator begin, Iterator end, size_t page_size) {
     assert(end >= begin);
+    assert(page_size > 0);
     for (size_t left = std::distance(begin, end); left > 0;) {
-      assert(page_size > 0);
       const size_t current_page_size = std::min(page_size, left);
       const Iterator current_page_end = std::next(begin, current_page_size);
       pages_.push_back({begin, current_page_end});
